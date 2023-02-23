d57b557c2e10997ca80326d1e82fa8977a35009c
Robert Li <1067907540@qq.com>
2023-02-22 19:26:39 -0700

    Part 1 2 4 5 finished

--- app/src/main/java/com/example/lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/example/lifestyleapp/MainActivity.kt
@@ -16,8 +16,6 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.provider.MediaStore
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -33,6 +31,7 @@ const val sex_text = "SX_TEXT"
 const val country_text = "CRT_TEXT"
 const val city_text = "CT_TEXT"
 const val activity_text = "ACT_TEXT"
+const val activity_textView = "ACT_TEXTVIEW"
 class MainActivity : AppCompatActivity() ,View.OnClickListener{
     //updated
     private var age_Input: EditText? = null
@@ -47,6 +46,8 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
     private var mButtonBMR: Button? = null
     private var mButtonPro: Button? = null
     private var mButtonHikes: Button? = null
+    private var mButtonCalculate: Button?=null
+    private var tv_intake: TextView?= null
 
     //pics
     private lateinit var takePicLauncher: ActivityResultLauncher<String>
@@ -61,18 +62,13 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
     private var mCountry: String? = null
     private var mCity: String? = null
     private var mlvl: String? = null
+    private var mIntake:String? = null
 
-    private var suggestions_Act = arrayOf("High","Medium","Low")
+    //private var suggestions_Act = arrayOf("High","Medium","Low")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var adapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,suggestions_Act)
-        val autocompletetextview = findViewById<AutoCompleteTextView>(R.id.autoComplete_act)
-        autocompletetextview.threshold=0
-        autocompletetextview.setAdapter(adapter)
-
 
         age_Input = findViewById(R.id.et_Age)
         name_Input = findViewById(R.id.et_Name)
@@ -83,11 +79,13 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
         country_Input = findViewById(R.id.et_Country)
         city_Input = findViewById(R.id.et_City)
         imageView = findViewById(R.id.image)
+        tv_intake = findViewById(R.id.intake)
 
         mButtonCamera = findViewById(R.id.button_pic)
         mButtonBMR = findViewById(R.id.button_BMR)
         mButtonPro = findViewById(R.id.button_Profile)
         mButtonHikes = findViewById(R.id.button_hikes)
+        mButtonCalculate = findViewById(R.id.button_calculate)
 
         takePicLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
             Log.d("MainActivity", "onCreate: uri=$it, ")
@@ -97,6 +95,7 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
         mButtonCamera!!.setOnClickListener(this)
         mButtonBMR!!.setOnClickListener(this)
         mButtonPro!!.setOnClickListener(this)
+        mButtonCalculate!!.setOnClickListener(this)
 
         imageView.setOnClickListener {
             takePicLauncher.launch("image/*")
@@ -127,7 +126,8 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
         mSexual = sex_Input!!.text.toString()
         mCountry = country_Input!!.text.toString()
         mCity = city_Input!!.text.toString()
-
+        mlvl = activity_Input!!.text.toString()
+        mIntake = tv_intake!!.text.toString()
 
         outState.putString(name_text,mStringName)
         outState.putString(age_text,mStringAge)
@@ -136,8 +136,8 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
         outState.putString(sex_text,mSexual)
         outState.putString(country_text,mCountry)
         outState.putString(city_text,mCity)
-
-
+        outState.putString(activity_text,mlvl)
+        outState.putString(activity_textView,mIntake)
 
 
         if(imageView!!.drawable != null){
@@ -149,13 +149,14 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         name_Input!!.setText(savedInstanceState.getString(name_text))
-        age_Input!!.setText(savedInstanceState.getString(name_text))
+        age_Input!!.setText(savedInstanceState.getString(age_text))
         height_Input!!.setText(savedInstanceState.getString(height_text))
         weight_Input!!.setText(savedInstanceState.getString(weight_text))
         sex_Input!!.setText(savedInstanceState.getString(sex_text))
         city_Input!!.setText(savedInstanceState.getString(city_text))
         country_Input!!.setText(savedInstanceState.getString(country_text))
-
+        activity_Input!!.setText(savedInstanceState.getString(activity_text))
+        tv_intake!!.text = savedInstanceState.getString(activity_textView)
 
         if(Build.VERSION.SDK_INT >= 33){
             imageView!!.setImageBitmap(savedInstanceState.getParcelable("BITMAP", Bitmap::class.java))
@@ -167,6 +168,67 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
 
     override fun onClick(view: View?) {
         when(view?.id){
+            R.id.button_weather->{
+
+            }
+            R.id.button_calculate ->{
+                tv_intake = findViewById(R.id.intake)
+                height_Input = findViewById(R.id.et_Height)
+                weight_Input = findViewById(R.id.et_Weight)
+                sex_Input = findViewById(R.id.et_sex)
+                age_Input = findViewById(R.id.et_Age)
+                activity_Input = findViewById(R.id.autoComplete_act)
+
+                mHeight = height_Input!!.text.toString()
+                mWeight = weight_Input!!.text.toString()
+                mSexual = sex_Input!!.text.toString()
+                mStringAge = age_Input!!.text.toString()
+                mlvl = activity_Input!!.text.toString()
+
+                var weightV = 0.0
+                var heightV = 0.0
+                var ageV = 0.0
+
+                if(mWeight != ""){
+                    weightV = mWeight!!.toDouble()
+                }
+                if(mHeight != ""){
+                    heightV = mHeight!!.toDouble()
+                }
+                if(mStringAge != ""){
+                    ageV = mStringAge!!.toDouble()
+                }
+
+                var bmr = ""
+                var amr = ""
+                if(mSexual == "Men"){
+                    bmr = String.format("%.3f", 88.362 + (13.397 * weightV) + (4.799 * heightV) - (5.677 * ageV))
+                }else if(mSexual == "Women"){
+                    bmr = String.format("%.3f", 447.593 + (9.247 * weightV) + (3.098 * heightV) - (4.330 * ageV))
+                }
+
+                if(mlvl == "S"){
+                    amr = String.format("%.3f", bmr.toDouble() * 1.2)
+                }else if(mlvl == "Lightly active "){
+                    amr = String.format("%.3f", bmr.toDouble() * 1.375)
+                }else if(mlvl == "Moderately active"){
+                    amr = String.format("%.3f", bmr.toDouble() * 1.55)
+                }else if(mlvl == "Active"){
+                    amr = String.format("%.3f", bmr.toDouble() * 1.725)
+                }else if(mlvl == "Very activity"){
+                    amr = String.format("%.3f", bmr.toDouble() * 1.9)
+                }
+
+                if(mHeight.isNullOrBlank() || mWeight.isNullOrBlank() ||
+                    mSexual.isNullOrBlank() || mStringAge.isNullOrBlank() || mlvl.isNullOrBlank()){
+                    Toast.makeText(this@MainActivity, "enter all height, weight, age , sex and your activity level", Toast.LENGTH_SHORT).show()
+
+                } else{
+                    tv_intake!!.text = amr
+                }
+
+            }
+
             R.id.button_pic ->{
                 //The button press should open a camera
                 val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -195,10 +257,8 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
                 mCountry = country_Input!!.text.toString()
                 mlvl = activity_Input!!.text.toString()
 
-                if(mHeight.isNullOrBlank() || mWeight.isNullOrBlank() || mStringAge.isNullOrBlank() ||
-                    mSexual.isNullOrBlank() || mCity.isNullOrBlank() || mStringName.isNullOrBlank() ||
-                    mCountry.isNullOrBlank() || mlvl.isNullOrBlank()){
-                    Toast.makeText(this@MainActivity, "enter all information", Toast.LENGTH_SHORT).show()
+                if(mStringName.isNullOrBlank()){
+                    Toast.makeText(this@MainActivity, "at least enter your name", Toast.LENGTH_SHORT).show()
                 }else{
                     Toast.makeText(this@MainActivity, "Welcome!\"", Toast.LENGTH_SHORT).show()
                     val messageIntent = Intent(this, Profile::class.java)
@@ -218,31 +278,47 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
                 weight_Input = findViewById(R.id.et_Weight)
                 sex_Input = findViewById(R.id.et_sex)
                 age_Input = findViewById(R.id.et_Age)
+
                 mHeight = height_Input!!.text.toString()
                 mWeight = weight_Input!!.text.toString()
                 mSexual = sex_Input!!.text.toString()
                 mStringAge = age_Input!!.text.toString()
-                var weightV = mWeight!!.toDouble()
-                var heightV = mHeight!!.toDouble()
-                var ageV = mStringAge!!.toDouble()
-                var bmr = ""
-                if(mSexual == "M"){
-                    bmr = String.format("%.3f", 88.362 + (13.397 * weightV) + (4.799* heightV) - (5.677*ageV))
-                }else if(mSexual == "W"){
-                    bmr = String.format("%.3f", 447.593 + (9.247 * weightV) + (3.098* heightV) - (4.330*ageV))
+
+                var weightV = 0.0
+                var heightV = 0.0
+                var ageV = 0.0
+                if(mWeight != ""){
+                    weightV = mWeight!!.toDouble()
                 }
+                if(mHeight != ""){
+                    heightV = mHeight!!.toDouble()
+                }
+                if(mStringAge != ""){
+                    ageV = mStringAge!!.toDouble()
+                }
+                var bmr = ""
+
 
                 if(mHeight.isNullOrBlank() || mWeight.isNullOrBlank() || mStringAge.isNullOrBlank() || mSexual.isNullOrBlank()){
                     Toast.makeText(this@MainActivity, "enter all height, weight, age and sex", Toast.LENGTH_SHORT).show()
                 }else{
+                    if(mSexual == "Men"){
+                        bmr = String.format("%.3f", 88.362 + (13.397 * weightV) + (4.799* heightV) - (5.677*ageV))
+                    }else if(mSexual == "Women"){
+                        bmr = String.format("%.3f", 447.593 + (9.247 * weightV) + (3.098* heightV) - (4.330*ageV))
+                    }
                     Toast.makeText(this@MainActivity, "Welcome!\"", Toast.LENGTH_SHORT).show()
                     val messageIntent = Intent(this, BMR::class.java)
                     messageIntent.putExtra("BMR", bmr)
                     this.startActivity(messageIntent)
                 }
+
+
             }
         }
     }
+
+
     private val cameraActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
         if(result.resultCode == RESULT_OK) {
 
