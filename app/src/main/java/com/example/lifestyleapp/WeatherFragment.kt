84a65374f71f3763e853bcca026cf0c82812db73
CarterRigert <89533142+CarterRigert@users.noreply.github.com>
2023-04-24 20:33:27 -0600

    fixed weather fragment

--- app/src/main/java/com/example/lifestyleapp/WeatherFragment.kt
+++ app/src/main/java/com/example/lifestyleapp/WeatherFragment.kt
@@ -1,142 +1,107 @@
-package com.example.lifestyleapp;
+package com.example.lifestyleapp
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.os.HandlerCompat;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.util.Log;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.json.JSONException;
-
-import java.lang.ref.WeakReference;
-import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-
-public class WeatherFragment extends Fragment implements View.OnClickListener{
+class WeatherFragment : Fragment(), View.OnClickListener {
+    private var mEtLocation: EditText? = null
+    private var mTvTemp: TextView? = null
+    private var mTvPress: TextView? = null
+    private var mTvHum: TextView? = null
+    private val mWeatherData: WeatherData? = null
+    private var mBtSubmit: Button? = null
+    private var mWeatherViewModel: WeatherViewModel? = null
+    private val userViewModel: UserViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
 
-    private EditText mEtLocation;
-    private TextView mTvTemp;
-    private TextView mTvPress;
-    private TextView mTvHum;
-    private WeatherData mWeatherData;
-    private Button mBtSubmit;
-
-    private WeatherViewModel mWeatherViewModel;
-
+        val userId = 1 // Replace with the actual user ID
+        userViewModel.setUserId(userId)
+        userViewModel.getUser(userId).observe(viewLifecycleOwner, Observer { user ->
+            if(user != null) {
+                if(user.city != null){
+                    mEtLocation!!.setText(user.city)
+                }
 
 
-    public WeatherFragment() {
-        // Required empty public constructor
-    }
+            }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        String strCity = getArguments().getString("city");
+        })
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        val view = inflater.inflate(R.layout.fragment_weather, container, false)
         //Get the edit text and all the text views
-        mEtLocation = (EditText) view.findViewById(R.id.et_location);
-        mTvTemp = (TextView) view.findViewById(R.id.tv_temp);
-        mTvPress = (TextView) view.findViewById(R.id.tv_pressure);
-        mTvHum = (TextView) view.findViewById(R.id.tv_humidity);
-
-
-
-        if(savedInstanceState!=null) {
-            String Lo = savedInstanceState.getString("loc");
-            String temp = savedInstanceState.getString("tvTemp");
-            String hum = savedInstanceState.getString("tvHum");
-            String press = savedInstanceState.getString("tvPress");
-            if( Lo != null)
-                mEtLocation.setText(Lo);
-            if (temp != null)
-                mTvTemp.setText(""+temp);
-            if (hum != null)
-                mTvHum.setText(""+hum);
-            if (press != null)
-                mTvPress.setText(""+press);
+        mEtLocation = view.findViewById<View>(R.id.et_location) as EditText
+        mTvTemp = view.findViewById<View>(R.id.tv_temp) as TextView
+        mTvPress = view.findViewById<View>(R.id.tv_pressure) as TextView
+        mTvHum = view.findViewById<View>(R.id.tv_humidity) as TextView
+        if (savedInstanceState != null) {
+            val Lo = savedInstanceState.getString("loc")
+            val temp = savedInstanceState.getString("tvTemp")
+            val hum = savedInstanceState.getString("tvHum")
+            val press = savedInstanceState.getString("tvPress")
+            if (Lo != null) mEtLocation!!.setText(Lo)
+            if (temp != null) mTvTemp!!.text = "" + temp
+            if (hum != null) mTvHum!!.text = "" + hum
+            if (press != null) mTvPress!!.text = "" + press
         }
 
-        mEtLocation.setText(strCity);
-
-
-
-        mBtSubmit = (Button) view.findViewById(R.id.button_submit);
-        mBtSubmit.setOnClickListener(this);
+        mBtSubmit = view.findViewById<View>(R.id.button_submit) as Button
+        mBtSubmit!!.setOnClickListener(this)
 
 
         //Create the view model
-        mWeatherViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
+        mWeatherViewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
 
         //Set the observer
-        (mWeatherViewModel.getData()).observe(getViewLifecycleOwner(),nameObserver);
-
-
-        return view;
+        mWeatherViewModel!!.data.observe(viewLifecycleOwner, nameObserver)
+        return view
     }
 
     //create an observer that watches the LiveData<WeatherData> object
-    final Observer<WeatherData> nameObserver  = new Observer<WeatherData>() {
-        @Override
-        public void onChanged(@Nullable final WeatherData weatherData) {
-            // Update the UI if this data variable changes
-            if(weatherData!=null) {
-                mTvTemp.setText("" + Math.round(weatherData.getTemperature().getTemp() - 273.15) + " C");
-                mTvHum.setText("" + weatherData.getCurrentCondition().getHumidity() + "%");
-                mTvPress.setText("" + weatherData.getCurrentCondition().getPressure() + " hPa");
+    val nameObserver: Observer<WeatherData?> =
+        Observer<WeatherData?> { weatherData -> // Update the UI if this data variable changes
+            if (weatherData != null) {
+                mTvTemp!!.text = "" + Math.round(weatherData.temperature.temp - 273.15) + " C"
+                mTvHum!!.text = "" + weatherData.currentCondition.humidity + "%"
+                mTvPress!!.text = "" + weatherData.currentCondition.pressure + " hPa"
             }
         }
-    };
-    @Override
-    public void onClick(View view) {
 
-        switch(view.getId()){
-            case R.id.button_submit:{
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_submit -> {
+
                 //Get the string from the edit text and sanitize the input
-                String inputFromEt = mEtLocation.getText().toString().replace(' ','&');
-                loadWeatherData(inputFromEt);
+                val inputFromEt = mEtLocation!!.text.toString().replace(' ', '&')
+                loadWeatherData(inputFromEt)
             }
-
-            break;
         }
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("loc",mEtLocation.getText().toString());
-        outState.putString("tvTemp",mTvTemp.getText().toString());
-        outState.putString("tvHum",mTvHum.getText().toString());
-        outState.putString("tvPress",mTvPress.getText().toString());
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("loc", mEtLocation!!.text.toString())
+        outState.putString("tvTemp", mTvTemp!!.text.toString())
+        outState.putString("tvHum", mTvHum!!.text.toString())
+        outState.putString("tvPress", mTvPress!!.text.toString())
     }
 
-
-
-
-    private void loadWeatherData(String location){
-        mWeatherViewModel.setLocation(location);
+    private fun loadWeatherData(location: String) {
+        mWeatherViewModel!!.setLocation(location)
     }
-
-
-
-
 }
